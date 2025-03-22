756923c9579a4d4c05adf45e39eb08b3712410c3
Mikhail Shikhalev <shikhalev.mike@gmail.com>
2025-03-22 18:58:38 +0400

    V0.0.1

--- backend/src/main/java/com/example/lab2/controller/RedirectController.java
+++ backend/src/main/java/com/example/lab2/controller/RedirectController.java
@@ -1,11 +1,11 @@
 package com.example.lab2.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
+@RequestMapping("/projects")
 public class RedirectController {
 
     @RequestMapping(value = {"/", "/{path:[^\\.]*}", "/projects/**"})
